820441eb3b11341d8e52e5fbb9b0592ba8796e4f
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-03-13 14:29:07 -0300

    Ejercicio terminado
    Se utilizo componente de react para cambiar colores(estilo) segun el usuario haga click en cada luz(evento onclick)+(cambio de state).

--- src/components/semaforo.jsx
+++ src/components/semaforo.jsx
@@ -2,21 +2,49 @@ import React from 'react';
 
 class Luces extends React.Component {
     constructor() {
-        super(); this.state = {
-            active: ''
+        super();
+        this.state = {
+            estadoactualR: 'off',
+            estadoactualA: 'off',
+            estadoactualV: 'off'
         }
     }
 
+    cambiaColor = e => {
+        if (e.target.id == 'idRojo') {
+            this.setState({
+                estadoactualR: 'on',
+                estadoactualA: 'off',
+                estadoactualV: 'off'
+            })
+        } else if (e.target.id == 'idAmarillo') {
+            this.setState({
+                estadoactualA: 'on',
+                estadoactualR: 'off',
+                estadoactualV: 'off'
+            })
+        } else if (e.target.id == 'idVerde') {
+            this.setState({
+                estadoactualV: 'on',
+                estadoactualR: 'off',
+                estadoactualA: 'off'
+            })
+        }
+    }
 
-    render(){
-        return ( /*hacer evento click aki*/
-
+    render() {
+        return (
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <div className="alert alert-danger rounded-circle" role="alert"></div>
-                        <div className="alert alert-warning rounded-circle" role="alert"></div>
-                        <div className="alert alert-success rounded-circle" role="alert"></div>
+                        <div id="idRojo" className={"alert " + (this.state.estadoactualR == 'on' ? 'alert-danger1' : 'alert-danger ') + " rounded-circle"}
+                            onClick={e => this.cambiaColor(e)} ></div>
+
+                        <div id="idAmarillo" className={"alert " + (this.state.estadoactualA == 'on' ? 'alert-warning1' : 'alert-warning ') + " rounded-circle"}
+                            onClick={e => this.cambiaColor(e)}></div>
+
+                        <div id="idVerde" className={"alert " + (this.state.estadoactualV == 'on' ? 'alert-success1' : 'alert-success ') + " rounded-circle"}
+                            onClick={e => this.cambiaColor(e)}></div>
                     </div>
                 </div>
             </div>
